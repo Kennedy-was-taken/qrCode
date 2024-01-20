95427c3ee244a0613e873e6e9ea819493a3a2757
Kennedy <Tuks1032@gmail.com>
2024-01-21 00:45:30 +0200

    paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QrformComponent } from './qrform/qrform.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: QrformComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
